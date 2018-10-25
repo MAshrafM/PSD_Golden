b323ba1ba95f3ff850283b6c5459a915bc725b60
MAshraf <ee.mashraf@gmail.com>
2018-10-25 15:36:08 +0200

    services

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles/main.scss";
 
+$(window).scroll(function(){
+  if($(window).scrollTop() > (0.75 * $(window).height())){
+    $("nav").addClass("bg-dark-scroll");
+  } else {
+    $("nav").removeClass("bg-dark-scroll");
+  }
+});
