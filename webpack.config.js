65be7fdc9af36a00f599c72df7782f05b7ea6e31
MAshraf <ee.mashraf@gmail.com>
2018-10-25 14:11:58 +0200

    init GoldenStudio

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  module:{
+    rules: [
+    {
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['@babel/preset-env']
+      }
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader','sass-loader']
+    },
+    {
+      test: /\.(gif|png|jpg|svg)$/i,
+      loader: 'image-webpack-loader',
+      enforce: 'pre',
+      options: {
+        disable: false
+      }
+    },
+    {
+      test: /\.(gif|png|jpg|svg)$/i,
+      exclude: /node_modules/,
+      loader: 'url-loader',
+      options: {
+        limit: 8192
+      }
+    },
+    {
+      test: /\.(html)$/,
+      use: [{
+        loader: 'html-loader'
+      }]
+    }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(
+      {filename: 'style.css'}
+    ),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
